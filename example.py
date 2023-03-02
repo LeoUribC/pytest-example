34e2a3b9eaf31d6f7765975d1ad0675cc80b9723
LeoUribe <leo.uribe1205@gmail.com>
2023-03-02 15:02:52 -0500

    test added

--- example.py
+++ example.py
@@ -1,6 +1,13 @@
 def add(a,b):
 	return a+b
 
+def substract(a, b):
+	return a-b
+
 def test_add():
 	assert add(0.1, 0.2) >= 0.3
 	assert add("space", "ship") == "spaceship"
+"""
+def test_substract():
+	assert substract(1, 2) == -1
+"""
